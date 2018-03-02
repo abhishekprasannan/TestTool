a84be63d767fd4d29f0c7caf8bbe3c02872a435b
abhishekprasannan <abhishek.sica@gmail.com>
2018-03-02 20:17:26 +0530

    Test check GCD class

---
+++ Test/src/Gcd.java
@@ -0,0 +1,44 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Gcd {
+	private static int first(int a, int b){
+		if(b == 0){
+			return a;
+		} else{
+			return second(b , a-b);
+		}		
+	}
+	private static int second(int a, int b){
+		if(a == 0){
+			return b;
+		} else{
+			return first(b,a);
+		}
+	}
+	public static void main(String[] args) {
+//		System.out.println(first(5, 3));
+		
+		//===========
+		Map<Key, Integer> map = new HashMap<>();
+		Key firstKey  = new Key();
+		map.put(firstKey, 0);
+		for(int i=0; i< Integer.parseInt(args[0]);i++){
+			map.put(new Key(), i);
+		}
+		map.get(firstKey);
+	}
+	
+	//============================
+	
+	static class Key{
+
+		@Override
+		public int hashCode() {
+			// TODO Auto-generated method stub
+			return 42;
+		}
+		
+	}
+}
